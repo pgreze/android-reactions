ad8ed1a883a294ac252b170d10bf9a8382b1a0ba
pgreze <pierrick.greze@gmail.com>
2019-05-01 19:33:26 +0900

    Migrate to maven-publish

--- buildSrc/src/main/kotlin/Publish.kt
+++ buildSrc/src/main/kotlin/Publish.kt
@@ -6,18 +6,10 @@
  * https://inthecheesefactory.com/blog/how-to-upload-library-to-jcenter-maven-central-as-dependency/en
  */
 object Publish {
-    const val publishedGroupId = "com.github.pgreze"
-    const val libraryName = "Android Reactions"
-    const val artifact = "android-reactions"
+    const val groupId = "com.github.pgreze"
+    const val artifactId = "android-reactions"
+    const val version = "1.0"
 
     const val siteUrl = "https://github.com/pgreze/android-reactions"
     const val gitUrl = "$siteUrl.git"
-
-    const val libraryVersion = "1.0"
-
-    const val developerId = "pgreze"
-    const val developerName = "Pierrick Greze"
-
-    const val licenseName = "The Apache Software License, Version 2.0"
-    const val licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0.txt"
 }
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,10 +1,12 @@
 import com.android.build.gradle.LibraryExtension
+import groovy.util.Node
+import org.gradle.api.publish.maven.MavenPom
 
 plugins {
     id("com.android.library")
     kotlin("android")
+    `maven-publish`
 }
-apply(from = "../gradle/publish.gradle")
 
 configure<LibraryExtension> {
     compileSdkVersion(Config.targetSdk)
@@ -25,4 +27,47 @@ dependencies {
     "api"(Libs.support)
 }
 
-apply(from = "../gradle/install.gradle")
+// Publishing
+
+group = Publish.groupId
+version = Publish.version
+
+val androidSourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(android.sourceSets.getByName("main").java.srcDirs)
+}
+
+afterEvaluate {
+    publishing {
+        publications.invoke {
+            create("maven", MavenPublication::class) {
+                artifactId = Publish.artifactId
+
+                artifact(tasks.getByName("bundleReleaseAar"))
+                artifact(androidSourcesJar.get())
+
+                pom.addDependencies()
+            }
+        }
+    }
+}
+
+fun MavenPom.addDependencies() = withXml {
+    asNode().appendNode("dependencies").let { deps ->
+        // List all "compile" dependencies (for old Gradle)
+        configurations.compile.get().dependencies.addDependencies(deps, "compile")
+        // List all "api" dependencies (for new Gradle) as "compile" dependencies
+        configurations.api.get().dependencies.addDependencies(deps, "compile")
+        // List all "implementation" dependencies (for new Gradle) as "runtime" dependencies
+        configurations.implementation.get().dependencies.addDependencies(deps, "runtime")
+    }
+}
+
+fun DependencySet.addDependencies(node: Node, scope: String) = forEach {
+    node.appendNode("dependency").apply {
+        appendNode("groupId", it.group)
+        appendNode("artifactId", it.name)
+        appendNode("version", it.version)
+        appendNode("scope", scope)
+    }
+}
