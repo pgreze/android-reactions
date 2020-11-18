18b82a2dd3fffd4a1125cd88feb9e6a04112fb13
Harsh Rajgor <harshrajgor996@gmail.com>
2020-11-18 19:59:23 +0530

    added support for changing alpha value and changing corner radius of the popup (#22)
    * added support for changing alpha value and changing corner radius of the popup
    
    * updated readme with info for – popupAlphaValue and cornerSizeInDp

--- library/src/main/java/com/github/pgreze/reactions/RoundedView.kt
+++ library/src/main/java/com/github/pgreze/reactions/RoundedView.kt
@@ -4,9 +4,9 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.graphics.Path
 import android.graphics.RectF
 import android.util.Log
+import android.util.TypedValue
 import android.view.View
 
 /**
@@ -20,63 +20,28 @@ class RoundedView(context: Context, private val config: ReactionsConfig) : View(
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         color = config.popupColor
         style = Paint.Style.FILL
-        alpha = 230
+        alpha = config.popupAlphaValue
     }
 
-    private var cornerSize = 0f
-
-    private var xStart = 0f
-    private var yStart = 0f
+    private var offset = 0f
 
     override fun onSizeChanged(w: Int, h: Int, oldW: Int, oldH: Int) {
         Log.d(tag, "onSizeChanged: w = $w; h = $h; oldW = $oldW; oldH = $oldH")
-
-        val xPad = if (paddingLeft + paddingRight <= 0) {
-            config.horizontalMargin * 2f
-        } else {
-            (paddingLeft + paddingRight) / 2f
-        }
-        val bPad = xPad / 2
-        val nIcons = config.reactions.size
-        val regIconSize = (w - (2 * xPad + (nIcons - 1) * bPad)) / nIcons
-        cornerSize = xPad + regIconSize / 2
-        xStart = cornerSize
-        yStart = 0f
-
+        offset = 0f
         Log.d(tag, "onSizeChanged: padding left = " + paddingLeft + "; padding right = " + paddingRight +
                 "; padding top = " + paddingTop + "; padding bottom = " + paddingBottom)
-        Log.d(tag, "onSizeChanged: xStart = " + (x + xStart) + "; cornerSize = " + cornerSize + "; x = " + x)
+        Log.d(tag, "onSizeChanged: xStart = " + (x + offset) + "; cornerSize = " + config.cornerSizeInDp + "; x = " + x)
     }
 
-    private val path = Path()
     private val rect = RectF()
 
     override fun onDraw(canvas: Canvas) {
         // Draw the background rounded rectangle
-        path.moveTo(xStart, yStart)
-
-        // Top line between curves
-        path.lineTo(xStart + (width - 2 * cornerSize), yStart)
-
-        // First curve, right side
-        rect.left = xStart + width - 2 * cornerSize
-        rect.right = rect.left + cornerSize
-        rect.top = yStart
-        rect.bottom = yStart + height
-        path.arcTo(rect, 270f, 180f)
-
-        // Bottom line between curves
-        path.lineTo(xStart, yStart + height)
-
-        // Second curve, left side
-        rect.left = xStart - cornerSize
-        rect.right = xStart
-        rect.top = yStart
-        rect.bottom = yStart + height
-        path.arcTo(rect, 90f, 180f)
-        path.close()
-
-        canvas.drawPath(path, paint)
-        path.reset()
+        rect.left = offset
+        rect.right = width.toFloat() - offset
+        rect.top = offset
+        rect.bottom = height.toFloat() - offset
+        val cornerSizeInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, config.cornerSizeInDp.toFloat(), context.resources.displayMetrics)
+        canvas.drawRoundRect(rect, cornerSizeInPx, cornerSizeInPx, paint)
     }
 }
