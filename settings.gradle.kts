7eb57702e3e9dc29446d18bab286348c8bf1dacb
pgreze <pierrick.greze@gmail.com>
2021-04-21 19:17:44 +0900

    Add refreshVersions in settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
+import de.fayard.refreshVersions.bootstrapRefreshVersions
+
+buildscript {
+    repositories { gradlePluginPortal() }
+    dependencies.classpath("de.fayard.refreshVersions:refreshVersions:0.9.7")
+}
+
+bootstrapRefreshVersions()
+
 rootProject.name = "android-reactions"
 
 include(":library", ":sample")
